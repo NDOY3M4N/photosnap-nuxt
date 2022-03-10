4f9e49f2da5a0910b94f7bd28658016c4bb61f84
tenderking <tenderking@users.noreply.github.com>
2022-03-10 18:56:26 +0000

    deploy: eceaad6b2e388616324461c85184c41546b55ea3

---
+++ _nuxt/static/1646938585/features/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/features", {data:[{_img:{}}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1646938585/pricing/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fphotosnap-nuxt\u002F_nuxt\u002Fstatic\u002F1646938585",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpricing",config:{_app:{basePath:"\u002Fphotosnap-nuxt\u002F",assetsPath:"\u002Fphotosnap-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
